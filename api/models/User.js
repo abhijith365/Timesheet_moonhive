b9c67805b21ac214a209208009a205d4689c53c6
abijith365 <abhijithelampal005@gmail.com>
2022-09-07 13:56:12 +0530

    added user schema

---
+++ api/models/User.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const UserSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }, isAdmin: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+export default mongoose.model("User", UserSchema)
