a44ed2d9c8acd6d57ab4c279db073d0aa1f275ec
abijith365 <abhijithelampal005@gmail.com>
2022-09-08 22:42:28 +0530

    token verfication added

---
+++ api/utils/tokenVerfi.js
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken'
+
+export const verfiToken = (req, res, next) => {
+    const token = req.cookie.access_token
+
+    if (!token) return res.status(401).json({ "message": "Not authenticated" })
+
+    jwt.verify(token, process.env.JWR_SECRET, (err, user) => {
+        if (err) return req.status(404).json({ "message": "Token not valid" })
+
+        req.user = user
+        next()
+    })
+}
+
+// user varfiy
+export const userVerfi = (req, res, next) => {
+
+    verfiToken(req, res, next, () => {
+
+        const userId = req.user.id
+        const isAdmin = req.user.isAdmin
+
+        if (userId || isAdmin) {
+            next()
+        } else {
+            return res.status(403).json({ "message": "Your not authorized" })
+        }
+    })
+}
+
+// admin verify 
+export const adminVerfi = (req, res, next) => {
+    verfiToken(req, res, next, () => {
+        try {
+            const isAdmin = req.user.isAdmin
+            if (isAdmin) {
+                next()
+            } else {
+                return res.status(403).json({ "message": "Your not authorized" })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
+}
--- client/src/pages/user/Timesheet.jsx
+++ client/src/pages/user/Timesheet.jsx
@@ -4,22 +4,38 @@ export const Timesheet = () => {
     return (
         <>
             <div>
-                <p className="text-2xl font-semibold text-center">Monthly Timesheet</p>
+                <p className="text-2xl font-semibold text-center"> Timesheet</p>
             </div>
 
             <div className="flex justify-end mr-6">
 
-
                 <input name="start" type="date" class="bg-gray-50 border-slate-600	  text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date start" />
-
 
                 <div className="today-timesheet">
                     <button type="button" className="btn bg-red-500 text-white p-2 rounded-md" dataToggle="modal" >Add New Task</button>
                 </div>
 
-
             </div>
 
+            <div class="flex bg-sky-400 mx-5 mt-3 p-2 rounded text-white">
+
+                <div class="flex-1">
+                    <div class="projectnametitle">Task Name</div>
+                </div>
+                <div class="flex-1">
+                    <div class="completiontitle">Date</div>
+                </div>
+
+                <div class="flex-1 ">
+                    Duration
+                </div>
+                <div class="flex-1 mr-5">
+                    <div class="detailstitle">Description</div>
+                </div>
+                <div class="col-md-1 col-sm-1 col-xs-1">
+                    <div class="tsdelete-row"></div>
+                </div>
+            </div>
 
 
         </>
